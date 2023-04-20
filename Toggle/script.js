ebe85f46aa3d86f45cf83488b7ee834d8beecfa1
Martin Vidal Freyre <martinvidal.n7@gmail.com>
2023-04-20 15:36:58 -0300

    dark theme y menu

---
+++ Toggle/script.js
@@ -0,0 +1,15 @@
+let toggleBtn = document.querySelector(".toggleBtn")
+let container = document.querySelector(".container")
+
+
+
+toggleBtn.onclick = function() {
+    container.classList.toggle("active")
+}
+
+
+
+
+
+
+
